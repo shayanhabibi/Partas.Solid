ef9591d67a91d1128fbf4d2b4246532228cf08d3
cabboose <shayanftw@gmail.com>
2025-03-16 13:30:48 +0800

    Feature: `[<PartasImport({selector},{path})>]` replaces `[<Import({selector},{path}>]` for types with builder computations 0.2.17

---
+++ Partas.Solid.Tests/AttributeCases.fs
@@ -0,0 +1,12 @@
+﻿module AttributeCases
+
+open Xunit
+open Partas.Solid.Tests.Common
+
+let runAttributeCase caseName =
+    let folderName = "AttributeCases"
+    runCase folderName caseName
+
+[<Fact>]
+let ``PartasImport Attribute`` () =
+    runAttributeCase "PartasImportAttr"
---
+++ Partas.Solid.Tests/AttributeCases/PartasImportAttr/PartasImportAttr.fs
@@ -0,0 +1,15 @@
+﻿module Partas.Solid.Tests.AttributeCases.PartasImportAttr
+
+open Partas.Solid
+open Partas.Solid.Router
+open Fable.Core
+
+// todo-replace with a better test
+[<Erase>]
+type PartasImportAttr() =
+    inherit VoidNode()
+    [<SolidTypeComponentAttribute>]
+    member props.constructor =
+        Router() {
+            Route()
+        }
