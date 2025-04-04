8eac67f3dc8a13b4ffea1b37c1dc138f2c3afbba
shayanhabibi <caBboose@8>
2025-04-03 18:32:14 -0600

    Add unix/macosx test suite Fli compatibility

--- Partas.Solid.Tests/Common.fs
+++ Partas.Solid.Tests/Common.fs
@@ -3,61 +3,18 @@
 open Fli
 open FsUnitTyped
 open System.IO
+open System
 open Xunit
 
-// let START_TEST (name: string) = name; ()
-// let END_TEST (name: string) = name; ()
-//
-// let (|StartsWithTrimmed|_|) (comp: string) : string -> string option = function
-//     | value when value.StartsWith comp -> value.Substring(comp.Length, value.Length) |> Some
-//     | _ -> None
-// let (|Until|_|) (comp: char): string -> string option = function
-//     | line when line.Contains(comp) ->
-//         line
-//         |> _.Split(comp)
-//         |> Array.tryHead
-//     | _ -> None
-// let (|TestStart|_|): string -> string option = function
-//     | StartsWithTrimmed "START_TEST(" (Until ')' name) ->
-//         Some name
-//     | _ -> None
-// let (|TestEnd|_|): string -> unit option = function
-//     | StartsWithTrimmed "END_TEST" _ -> Some()
-//     | _ -> None
-//
-// let mutable testName = "None"
-//
-// let groupingFunctor: string -> string = function
-//     | TestStart name ->
-//         testName <- name
-//         "Dispose"
-//     | TestEnd ->
-//         testName <- "None"
-//         "Dispose"
-//     | _ -> testName
-//     
-// let getTestBlocks = File.ReadAllLines("") |> Array.groupBy groupingFunctor
-
-// type TestFolderFixture(folder: string) =
-//     let dir = $"{__SOURCE_DIRECTORY__}/{folder}"
-//     do
-//         cli {
-//             Shell CMD
-//             WorkingDirectory dir
-//             Command "dotnet fable --exclude Partas.Solid.FablePlugin --noCache -e .fs.jsx"
-//         }
-//         |> Command.execute
-//         |> Output.toExitCode
-//         |> shouldEqual 1
-//
-// [<CollectionDefinition("Build Collection")>]
-// type TestFolderCollection = ICollectionFixture<TestFolderFixture>
-
+let platformShell =
+    match Environment.OSVersion.Platform with
+    | PlatformID.Unix | PlatformID.MacOSX -> ZSH
+    | _ -> CMD
 
 let runCase folderName caseName =
     let dir = $"{__SOURCE_DIRECTORY__}/{folderName}/{caseName}"
     cli {
-        Shell CMD
+        Shell platformShell
         WorkingDirectory dir
         Command "dotnet fable --exclude Partas.Solid.FablePlugin --noCache -e .fs.jsx"
     }
