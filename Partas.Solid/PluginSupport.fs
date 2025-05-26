dfec53012ff67c5c247108454840bc70b8785560
cabboose <shayanftw@gmail.com>
2025-05-26 17:43:52 +0800

    Deprecate PartasProxyImportAttribute (because I never actually thought ImportAttribute was as featured as it was)

--- Partas.Solid/PluginSupport.fs
+++ Partas.Solid/PluginSupport.fs
@@ -3,6 +3,7 @@
 // Attributes that are matched in Partas.Solid.FablePlugin to support special transformations
 
 open System
+open Fable.Core
 
 /// Used for types that have inbuilt Builder computation support but are an imported
 /// component from an external library.
@@ -18,6 +19,7 @@ type PartasImportAttribute(selector: string, path: string) =
 /// proxies, but there is no reason to increase bundle sizes doing so.
 /// </summary>
 /// <param name="key">The extra parameter 'key' is '.' appended to the selector</param>
+[<Obsolete("Redundant. Importing with a dot selector actually is smarter than I thought. This now routes through Import")>]
 type PartasProxyImportAttribute(key: string, selector: string, path: string) =
-    inherit Attribute()
+    inherit ImportAttribute($"{selector}.{key}", path)
 
