47818b543c29f0b8e54cf14f45b1677d259b31eb
cabboose <shayanftw@gmail.com>
2025-07-06 18:09:58 +0800

    Create SolidStart w/ SolidBase doc block

---
+++ Partas.Solid/Program.fs.jsx
@@ -0,0 +1,10 @@
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { SolidBaseRoot } from "@kobalte/solidbase/client";
+
+export function Program() {
+    return <Router root={SolidBaseRoot}>
+        <FileRoutes />
+    </Router>;
+}
+
